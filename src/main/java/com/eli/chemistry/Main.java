69719d3e0a51dfa29bc306ddfe705e9d969f7fe0
Elizabeth Velasco <j.elizabeth.v@gmail.com>
2016-08-22 12:19:27 -0500

    ELI: implementa método getSimbolo

--- src/main/java/com/eli/chemistry/Main.java
+++ src/main/java/com/eli/chemistry/Main.java
@@ -1,8 +1,21 @@
 package com.eli.chemistry;
 
+import com.eli.chemistry.enums.ElementoQuimico;
+import com.sun.org.apache.bcel.internal.ExceptionConstants;
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        String miElemento ="aluminio";
+
+        try {
+            System.out.println(ElementoQuimico.getSimbolo(miElemento));
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }
--- src/main/java/com/eli/chemistry/enums/ElementoQuimico.java
+++ src/main/java/com/eli/chemistry/enums/ElementoQuimico.java
@@ -1,29 +1,38 @@
 package com.eli.chemistry.enums;
 
 public enum ElementoQuimico {
-    HIDROGENO("Hidrógeno","H",1,SerieQuimica.NOMETAL),
-    HELIO("Helio","He",2, SerieQuimica.GAS),
-    LITIO("Litio","Li",3, SerieQuimica.ALCALINO),
-    BERILIO("Berilio","Be",4,SerieQuimica.ALCALINOTERREO),
-    BORO("Boro","B",5,SerieQuimica.METALOIDE),
-    CARBONO("Carbono","C",6,SerieQuimica.NOMETAL),
-    NITROGENO("Nitrógeno","N",7,SerieQuimica.NOMETAL),
-    OXIGENO("Oxígeno","O",8, SerieQuimica.NOMETAL),
-    FLUOR("Flúor","F",9, SerieQuimica.HALOGENO),
-    ALUMINIO("Aluminio","Al",13, SerieQuimica.METAL),
-    COBRE("Cobre","C",19,SerieQuimica.TRANSICION);
+    HIDROGENO("hidrogeno", "H", 1, SerieQuimica.NOMETAL),
+    HELIO("helio", "He", 2, SerieQuimica.GAS),
+    LITIO("hitio", "Li", 3, SerieQuimica.ALCALINO),
+    BERILIO("berilio", "Be", 4, SerieQuimica.ALCALINOTERREO),
+    BORO("boro", "B", 5, SerieQuimica.METALOIDE),
+    CARBONO("carbono", "C", 6, SerieQuimica.NOMETAL),
+    NITROGENO("nitrogeno", "N", 7, SerieQuimica.NOMETAL),
+    OXIGENO("oxigeno", "O", 8, SerieQuimica.NOMETAL),
+    FLUOR("fluor", "F", 9, SerieQuimica.HALOGENO),
+    ALUMINIO("Aluminio", "Al", 13, SerieQuimica.METAL),
+    COBRE("Cobre", "C", 19, SerieQuimica.TRANSICION);
 
     private String nombre;
     private String simbolo;
     private int numeroAtomico;
     private SerieQuimica serieQuimica;
 
+
     ElementoQuimico(String nombre, String simbolo, int numeroAtomico, SerieQuimica serieQuimica) {
-        this.nombre=nombre;
-        this.simbolo=simbolo;
-        this.numeroAtomico=numeroAtomico;
-        this.serieQuimica=serieQuimica;
+        this.nombre = nombre;
+        this.simbolo = simbolo;
+        this.numeroAtomico = numeroAtomico;
+        this.serieQuimica = serieQuimica;
     }
 
+    public static String getSimbolo(String nombre) {
+        nombre = nombre.toUpperCase();
+        try {
+            return ElementoQuimico.valueOf(nombre).simbolo;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("No se ha encontrado el elemento");
+        }
+    }
     
 }
